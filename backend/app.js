cdc39e1b0e555c5c53ef65c81085a8515bb2ba51
9ta1ls <rpeleh19@gmail.com>
2024-02-29 09:37:29 +0200

    Update app.js
    Змінив щоб якщо треба буде змінити порт, то не треба буде переписувати багато.

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
