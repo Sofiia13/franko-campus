d2670ec89991a90fbbdafc4999750e45ba3cd177
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-03-12 00:47:03 +0200

    feat!: added a check for the correctness of the email

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,141 +8,133 @@ const bcrypt = require("bcrypt");
 // // create application/json parser
 // const jsonParser = bodyParser.json();
 
-const { storeActivationCode, generateActivationCode, sendActivationEmail } = require('../services/activationMailService');
+const {
+  storeActivationCode,
+  generateActivationCode,
+  sendActivationEmail,
+  validateEmail,
+} = require("../services/activationMailService");
 
 router.post("/register", async (req, res) => {
-    const { reqUsername, reqPassword, reqEmail, reqUniversity } = req.body;
-  
-    try {
-      const existingUser = await users.findOne({ where: { username: reqUsername } });
-  
-      if (existingUser) {
-        return res
-          .status(400)
-          .json({ error: "Ім'я користувача вже зайняте." });
-      }
-  
-      const hashedPassword = await bcrypt.hash(reqPassword, 10);
-  
-      await users.create({
-        username: reqUsername,
-        password: hashedPassword,
-        email: reqEmail,
-        university: reqUniversity,
-        is_verified: false
-      });
+  const { reqUsername, reqPassword, reqEmail, reqUniversity } = req.body;
 
-      createdUser = await users.findOne({ where: { username: reqUsername } });
+  try {
+    const existingUser = await users.findOne({
+      where: { username: reqUsername },
+    });
 
-      if (!createdUser) {
-        return res
-          .status(500)
-          .json({ error: "Внутрішня помилка сервера під час роботи з БД." });
-      }
+    if (existingUser) {
+      return res.status(400).json({ error: "Ім'я користувача вже зайняте." });
+    }
 
-      const activationCode = generateActivationCode();
+    // перевірка чи правильно введена пошта
+    if (!validateEmail(reqEmail)) {
+      return res.status(500).json({ error: "Неправильно введена пошта." });
+    }
 
-      await storeActivationCode(createdUser.id, activationCode);
-  
-      sendActivationEmail(createdUser.email, activationCode);
-  
-      return res
-        .status(200)
-        .json({ success: true });
-        //після успішного виконання цього запиту користувача потрібно перекинути на сторінку "валідація"
-        //
+    const hashedPassword = await bcrypt.hash(reqPassword, 10);
 
-    } catch (error) {
-      console.error("Під час реєстрації виникла помилка:", error);
+    await users.create({
+      username: reqUsername,
+      password: hashedPassword,
+      email: reqEmail,
+      university: reqUniversity,
+      is_verified: false,
+    });
+
+    createdUser = await users.findOne({ where: { username: reqUsername } });
+
+    if (!createdUser) {
       return res
         .status(500)
-        .json({ error: "Внутрішня помилка сервера." });
+        .json({ error: "Внутрішня помилка сервера під час роботи з БД." });
     }
-  });
-  
-  router.post("/validate", async (req, res) => {
-    const { reqUsername, reqCode } = req.body;
-    const intCode = parseInt(reqCode);
-  
-    try {
-      const user = await users.findOne({ where: { username: reqUsername } });
-  
-      if (!user) {
-        return res
-          .status(400)
-          .json({ error: "Користувач не знайдений." });
-      }
 
-      if (user.is_verified == true) {
-        return res
-          .status(409)
-          .json({ info: "Користувач вже верифікований." });
-      }
+    const activationCode = generateActivationCode();
 
-      const storedCode = await verificationCodes.findOne({
-        where: { user_id: user.id, code: intCode },
-      });
-  
-      if (!storedCode) {
-        return res
-          .status(400)
-          .json({ error: "Код неправильний." });
-      }
+    await storeActivationCode(createdUser.id, activationCode);
 
-      await users.update({ is_verified: true }, {
+    sendActivationEmail(createdUser.email, activationCode);
+
+    return res.status(200).json({ success: true });
+    //після успішного виконання цього запиту користувача потрібно перекинути на сторінку "валідація"
+    //
+  } catch (error) {
+    console.error("Під час реєстрації виникла помилка:", error);
+    return res.status(500).json({ error: "Внутрішня помилка сервера." });
+  }
+});
+
+router.post("/validate", async (req, res) => {
+  const { reqUsername, reqCode } = req.body;
+  const intCode = parseInt(reqCode);
+
+  try {
+    const user = await users.findOne({ where: { username: reqUsername } });
+
+    if (!user) {
+      return res.status(400).json({ error: "Користувач не знайдений." });
+    }
+
+    if (user.is_verified == true) {
+      return res.status(409).json({ info: "Користувач вже верифікований." });
+    }
+
+    const storedCode = await verificationCodes.findOne({
+      where: { user_id: user.id, code: intCode },
+    });
+
+    if (!storedCode) {
+      return res.status(400).json({ error: "Код неправильний." });
+    }
+
+    await users.update(
+      { is_verified: true },
+      {
         where: {
           username: reqUsername,
         },
-      });
+      }
+    );
 
-      //видалення вериф. коду з бд
-      await verificationCodes.destroy({
-        where: {
-          code: reqCode
-        },
-      })
+    //видалення вериф. коду з бд
+    await verificationCodes.destroy({
+      where: {
+        code: reqCode,
+      },
+    });
 
-      return res
-        .status(200)
-        .json({ success: true });
-        //
-        //після успішного виконання цього запиту користувача потрібно перекинути на сторінку "успіх" (або головну)
-        //
-    } catch (error) {
-      console.error("Виникла помилка під час валідації:", error);
-      return res
-          .status(500)
-          .json({ error: "Внутрішня помилка сервера." });
+    return res.status(200).json({ success: true });
+    //
+    //після успішного виконання цього запиту користувача потрібно перекинути на сторінку "успіх" (або головну)
+    //
+  } catch (error) {
+    console.error("Виникла помилка під час валідації:", error);
+    return res.status(500).json({ error: "Внутрішня помилка сервера." });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { reqUsername, reqPassword } = req.body;
+
+    const user = await users.findOne({ where: { username: reqUsername } });
+
+    if (!user) {
+      return res.status(404).json({ error: "Такого користувача не існує." });
     }
-  });
-  
-  
-  router.post("/login", async (req, res) => {
-    try {
-      const { reqUsername, reqPassword } = req.body;
-  
-      const user = await users.findOne({ where: { username: reqUsername } });
-  
-      if (!user) {
-        return res
-          .status(404)
-          .json({ error: "Такого користувача не існує." });
-      }
 
-      if (user.is_verified == false) {
-        return res
-        .status(401)
-        .json({ error: "Користувач не верифікований." });
-      }
-  
-      const passwordMatch = await bcrypt.compare(reqPassword, user.password);
-  
-      if (!passwordMatch) {
-        return res
-          .status(400)
-          .json({ error: "Неправильне ім'я користувача або пароль." });
-      }
+    if (user.is_verified == false) {
+      return res.status(401).json({ error: "Користувач не верифікований." });
+    }
 
+    const passwordMatch = await bcrypt.compare(reqPassword, user.password);
+
+    if (!passwordMatch) {
+      return res
+        .status(400)
+        .json({ error: "Неправильне ім'я користувача або пароль." });
+    }
 
     //  jwt token
     //
@@ -151,7 +143,6 @@ router.post("/register", async (req, res) => {
     //     "importantsecret"
     //   );
     //
-
 
     return res.status(200).json({ success: true });
     //
--- backend/services/activationMailService.js
+++ backend/services/activationMailService.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const { users, verificationCodes } = require("../models");
 
 const generateActivationCode = () => {
@@ -15,7 +15,10 @@ const storeActivationCode = async (userId, code) => {
       code: code,
     });
   } catch (error) {
-    console.error("Виникла помилка під час зберігання коду верифікації:", error);
+    console.error(
+      "Виникла помилка під час зберігання коду верифікації:",
+      error
+    );
 
     //видалити також запис про користувача, якщо сталась помилка з кодом активації
     users.destroy({
@@ -27,30 +30,47 @@ const storeActivationCode = async (userId, code) => {
   }
 };
 
+// перевірка чи в користувача правильно записана пошта
+function validateEmail(email) {
+  const validDomain = ["gmail.com", "lnu.edu.ua"];
+  for (let i = 0; i < validDomain.length; i++) {
+    if (email.endsWith(validDomain[i])) {
+      return true;
+    }
+  }
+  console.error("Invalid email address:", email);
+  return false;
+}
+
 function sendActivationEmail(email, activationCode) {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: "Gmail",
     auth: {
-      user: 'frankocampus@gmail.com',
-      pass: 'shiw qvlg zldm dpyf', 
+      user: "frankocampus@gmail.com",
+      pass: "shiw qvlg zldm dpyf",
       //actual password - kT63pU12
     },
   });
 
   const mailOptions = {
-    from: 'Franko Campus Helpcenter',
+    from: "Franko Campus Helpcenter",
     to: email,
-    subject: 'Activation Code for Your Franko Campus Account',
+    subject: "Activation Code for Your Franko Campus Account",
     text: `Your activation code is: ${activationCode}`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error('Error sending email:', error);
+      console.error("Error sending email:", error);
     } else {
-      console.log('Email sent:', info.response);
+      console.log("Email sent:", info.response);
     }
   });
 }
 
-module.exports = { storeActivationCode, generateActivationCode, sendActivationEmail };
+module.exports = {
+  storeActivationCode,
+  generateActivationCode,
+  sendActivationEmail,
+  validateEmail,
+};
