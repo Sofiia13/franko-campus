b4a7b0cf8cea1fef5279bd690f002eebb677e2d1
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-03-17 14:10:40 +0200

    fix: deleted field for university

--- backend/models/profilesModel.js
+++ backend/models/profilesModel.js
@@ -1,36 +1,39 @@
 module.exports = (sequelize, DataTypes) => {
-    const profiles = sequelize.define("profiles", {
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        first_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        last_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        university: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        status: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        }}, {
-            timestamps: false
-        });
+  const profiles = sequelize.define(
+    "profiles",
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      // university: {
+      //     type: DataTypes.STRING,
+      //     allowNull: false,
+      // },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
 
+  profiles.associate = (models) => {
+    profiles.belongsTo(models.users, {
+      foreignKey: "user_id",
+      onDelete: "CASCADE",
+    });
+  };
 
-        profiles.associate = (models) => {
-            profiles.belongsTo(models.users, {
-                foreignKey: 'user_id', 
-                onDelete: 'CASCADE', 
-            });
-        };
-
-        
-    return profiles;
+  return profiles;
 };
