978991b1cfe39b9466ee1efab64bf3f4e4f78277
VIYD <oleksandr.poliakov@lnu.edu.ua>
2024-04-07 02:12:44 +0300

    fix: deleted unnecessary app.js limit

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(express.json({limit : "5mb"}));
+app.use(express.json({ }));
 app.use(cors());
 //app.use(express.urlencoded({limit: 2000000, extended: false}));
 
