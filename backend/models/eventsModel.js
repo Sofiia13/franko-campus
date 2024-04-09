c60c3d4f480adaa36199cec3eb545b388b1de226
VIYD <oleksandr.poliakov@lnu.edu.ua>
2024-04-09 14:49:31 +0300

    db changes 09/04

--- backend/models/eventsModel.js
+++ backend/models/eventsModel.js
@@ -11,23 +11,51 @@ module.exports = (sequelize, DataTypes) => {
         description: {
             type: DataTypes.TEXT,
             allowNull: false,
-        }});
+        },
+        date: {
+            type: DataTypes.DATEONLY,
+            allowNull: true,
+        },
+        time: {
+            type: DataTypes.TIME,
+            allowNull: true,
+        },
+        format: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        cost: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        }, 
+        type: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        }
+
+        //!!
+        //потрібно змінити allowNull на false після тестування в date/time
+        //
+        //також потрібно буде перевірити, наскільки легко підтягувати з сайту
+        //типи date та time, і, можливо, змінити на просто string/varchar
+        //
+    });
 
     events.associate = (models) => {
         events.hasMany(models.eventImages, {
-            foreignKey: 'event_id',  
-            onDelete: 'CASCADE', 
-        }),
-        events.belongsToMany(models.eventParticipants, { 
-            through: 'eventParticipant',
             foreignKey: 'event_id',
-            otherKey: 'user_id' 
+            onDelete: 'CASCADE',
         }),
-        events.belongsToMany(models.userBookmarks, { 
-            through: 'userBookmark',
-            foreignKey: 'event_id',
-            otherKey: 'user_id' 
-        });
+            events.belongsToMany(models.eventParticipants, {
+                through: 'eventParticipant',
+                foreignKey: 'event_id',
+                otherKey: 'user_id'
+            }),
+            events.belongsToMany(models.userBookmarks, {
+                through: 'userBookmark',
+                foreignKey: 'event_id',
+                otherKey: 'user_id'
+            });
     };
 
     return events;
