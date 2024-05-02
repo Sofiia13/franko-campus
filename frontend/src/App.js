e26133decdd54b355f9f7facbebfcf75e7242e84
VIYD <oleksandr.poliakov@lnu.edu.ua>
2024-05-02 11:11:19 +0300

    feat: search is working, but now without filters

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,35 @@
-import React, { useState, useRef, useEffect } from 'react';
-
-import LoginPage from './pages/LoginPage.js';
-import SignUpPage from './pages/SignUpPage.js';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HeaderComponent from './components/HeaderComponent';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 import ValidationPage from './pages/ValidationPage';
 import ProfilePage from './pages/ProfilePage';
 import AddEventPage from './pages/AddEventPage';
 import EventPage from './pages/EventPage';
-import SearchResultsPage from './pages/SearchResultsPage.js';
-
-import HeaderComponent from './components/HeaderComponent.jsx';
-
+import SearchResultsPage from './pages/SearchResultsPage';
 import './App.css';
 
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-
-
 function App() {
-
   return (
-    <div className="App">
-      {/* Header */}
-      <HeaderComponent />
-      {/*Pages*/}
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
+        {/* Header */}
+        <HeaderComponent />
+        {/*Pages*/}
         <Routes>
-          <Route path="/" element={<HomePage />} />          
+          <Route path="/" element={<HomePage />} />
           <Route path="/auth/signup" element={<SignUpPage />} />
           <Route path="/auth/validation" element={<ValidationPage />} />
           <Route path="/auth/login" element={<LoginPage />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/addevent" element={<AddEventPage />} />
           <Route path="/event/:id" element={<EventPage />} />
-          <Route path='/search' element={<SearchResultsPage />} />
+          <Route path="/search/:query" element={<SearchResultsPage />} />
         </Routes>
-      </BrowserRouter>
-
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
